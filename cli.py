e689a9fc614d149f03c301457f20b425f7d8c24a
Yogesh V R <yogi.yogesh922@gmail.com>
2025-08-28 17:15:09 +0530

    initial commit

--- cli.py
+++ cli.py
@@ -1,1 +1,19 @@
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import NestedCompleter
+
+completer = NestedCompleter.from_nested_dict({
+    "show": {
+        "version": None,
+        "clock": None,
+        "ip": {
+            "interface": {"brief"}
+        }
+    },
+    "exit": None,
+})
+
+def generate_prompt(user):
+    initial_prompt="#"
+    final_prompt = initial_prompt + "{user}"
+    return final_prompt
 
